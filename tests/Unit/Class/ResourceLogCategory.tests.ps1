7706a89d4fbeed5ff9be83faed3bca36917de495
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2024-03-28 12:49:36 +0100

    Refactor ResourceLogCategory.tests.ps1

--- tests/Unit/Class/ResourceLogCategory.tests.ps1
+++ tests/Unit/Class/ResourceLogCategory.tests.ps1
@@ -11,62 +11,62 @@ $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
 # Import the project module
 Import-Module $ProjectName
 
-BeforeAll {
-
-    # Define properties for valid and invalid test objects
-    $ValidObj1Properties = @{
-        SourceType       = "Az"
-        ContainerId      = "11111111-1111-1111-1111-111111111111"
-        ResourceTypeName = "Provider.Resource1/Type"
-    }
-    $ValidObj2Properties = @{
-        SourceType       = "Az"
-        ContainerId      = "22222222-2222-2222-2222-222222222222"
-        ResourceTypeName = "Provider.Resource2/Type"
-    }
-    $InvalidObjProperties = @{
-        SourceType       = "Az"
-        ContainerId      = "ObjectId"
-        ResourceTypeName = "Provider.Resource3/Type"
-    }
-    $IncompleteObjProperties = @{
-        SourceType  = "Az"
-        ContainerId = "00000000-0000-0000-0000-000000000000"
-    }
-
-    # Mock the Get-AzDiagnosticSettingCategory function to return predefined results for testing
-    Mock -CommandName 'Get-AzDiagnosticSettingCategory' -MockWith {
-        param($ResourceId)
-
-        if ($ResourceId -eq "11111111-1111-1111-1111-111111111111") {
-            return @(
-                @{ Name = "LogCategory1"; CategoryType = "Logs" },
-                @{ Name = "MetricCategory1"; CategoryType = "Metrics" }
-            )
-        }
-        else {
-            return @()
-        }
-    } -Verifiable
-
-    # Mock the Get-AzResource function to return predefined results for testing
-    Mock -CommandName 'Get-AzResource' -MockWith {
-        param($ResourceId)
-
-        if ($ResourceId -eq "11111111-1111-1111-1111-111111111111") {
-            return @{ ResourceType = "Provider.Resource1/Type" }
-        }
-        elseif ($ResourceId -eq "22222222-2222-2222-2222-222222222222") {
-            return @{ ResourceType = "Provider.Resource2/Type" }
-        }
-        else {
-            return @{ ResourceType = "Provider.Resource3/Type" }
-        }
-    } -Verifiable
-}
 # Define tests within the scope of the project module
 InModuleScope $ProjectName {
 
+    BeforeAll {
+
+        # Define properties for valid and invalid test objects
+        $ValidObj1Properties = @{
+            SourceType       = "Az"
+            ContainerId      = "11111111-1111-1111-1111-111111111111"
+            ResourceTypeName = "Provider.Resource1/Type"
+        }
+        $ValidObj2Properties = @{
+            SourceType       = "Az"
+            ContainerId      = "22222222-2222-2222-2222-222222222222"
+            ResourceTypeName = "Provider.Resource2/Type"
+        }
+        $InvalidObjProperties = @{
+            SourceType       = "Az"
+            ContainerId      = "ObjectId"
+            ResourceTypeName = "Provider.Resource3/Type"
+        }
+        $IncompleteObjProperties = @{
+            SourceType  = "Az"
+            ContainerId = "00000000-0000-0000-0000-000000000000"
+        }
+
+        # Mock the Get-AzDiagnosticSettingCategory function to return predefined results for testing
+        Mock -CommandName 'Get-AzDiagnosticSettingCategory' -MockWith {
+            param($ResourceId)
+
+            if ($ResourceId -eq "11111111-1111-1111-1111-111111111111") {
+                return @(
+                    @{ Name = "LogCategory1"; CategoryType = "Logs" },
+                    @{ Name = "MetricCategory1"; CategoryType = "Metrics" }
+                )
+            }
+            else {
+                return @()
+            }
+        } -Verifiable
+
+        # Mock the Get-AzResource function to return predefined results for testing
+        Mock -CommandName 'Get-AzResource' -MockWith {
+            param($ResourceId)
+
+            if ($ResourceId -eq "11111111-1111-1111-1111-111111111111") {
+                return @{ ResourceType = "Provider.Resource1/Type" }
+            }
+            elseif ($ResourceId -eq "22222222-2222-2222-2222-222222222222") {
+                return @{ ResourceType = "Provider.Resource2/Type" }
+            }
+            else {
+                return @{ ResourceType = "Provider.Resource3/Type" }
+            }
+        } -Verifiable
+    }
     # Describe the group of tests for the ResourceLogCategory class
     Describe 'ResourceLogCategory Class Unit Tests' {
         # Define setup actions to be performed before all tests
