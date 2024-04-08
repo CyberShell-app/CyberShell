dc7a78f103ea242dd88fcc7f7a65ca58a018070b
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2024-04-08 10:26:08 +0200

    Add Set-ScriptExecutionPreference function to set the script execution preference in PowerShell

--- src/Private/Set-ScriptExecutionPreference.ps1
+++ src/Private/Set-ScriptExecutionPreference.ps1
@@ -1,4 +1,30 @@
 function Set-ScriptExecutionPreference {
+    <#
+.SYNOPSIS
+   Sets the script execution preference.
+
+.DESCRIPTION
+   The Set-ScriptExecutionPreference function sets the script execution preference. It supports three levels of verbosity: "Information", "Verbose", and "Debug".
+
+.PARAMETER ExecutionPreference
+   The execution preference to be set. It can be one of the following: "Information", "Verbose", "Debug". Default is "Information".
+
+.EXAMPLE
+   Set-ScriptExecutionPreference -ExecutionPreference "Verbose"
+   Sets the script execution preference to "Verbose". This will enable verbose logging.
+
+.EXAMPLE
+   Set-ScriptExecutionPreference -ExecutionPreference "Debug"
+   Sets the script execution preference to "Debug". This will enable verbose and debug logging.
+
+.EXAMPLE
+   Set-ScriptExecutionPreference
+   Sets the script execution preference to the default "Information". This will disable verbose and debug logging.
+
+.NOTES
+   The function changes the script scope variables $script:VerbosePreference and $script:DebugPreference.
+#>
+
     [CmdletBinding()]
     param (
         [ValidateSet("Information", "Verbose", "Debug")]
