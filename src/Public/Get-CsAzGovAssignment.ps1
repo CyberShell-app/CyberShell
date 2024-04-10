7cc9119b551bcc9a37e34bc2f2a576eab7c3f1af
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2024-04-10 15:48:06 +0200

    Add OverdueOnly parameter to Get-CsAzGovAssignment.ps1 to filter results

--- src/Public/Get-CsAzGovAssignment.ps1
+++ src/Public/Get-CsAzGovAssignment.ps1
@@ -6,14 +6,14 @@ function Get-CsAzGovAssignment {
 .DESCRIPTION
     This function retrieves the list of security assessments from Azure and stores the governance assignments.
 
-.PARAMETER SubId
-    The subscription ID for which the security assessments are to be retrieved.
-
 .PARAMETER azAPICallConf
     A hashtable containing the configuration for the Azure API call.
 
 .PARAMETER CsEnvironment
     The environment for which the function is being run.
+
+.PARAMETER OverdueOnly
+    A switch parameter that retrieves only the overdue governance assignments.
 
 .EXAMPLE
     Get-CsAzGovAssignment -SubId "your-subscription-id" -azAPICallConf $yourConfig -CsEnvironment "your-environment"
@@ -30,13 +30,11 @@ function Get-CsAzGovAssignment {
 #>
 
     param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [string]$SubId,
 
-        [Parameter(Position = 1, Mandatory = $true)]
+        [Parameter(Position = 0, Mandatory = $true)]
         [System.Collections.Hashtable]$azAPICallConf,
 
-        [Parameter(Position = 2, Mandatory = $true)]
+        [Parameter(Position = 1, Mandatory = $true)]
         [string]$CsEnvironment,
 
         [Parameter()]
@@ -114,7 +112,31 @@ function Get-CsAzGovAssignment {
     Write-OutputPadded "$payLoad" -Type 'data' -IndentLevel 1 -BlankLineBefore
 
     $uri = "$($azapicallconf.azAPIEndpointUrls.ARM)/providers/Microsoft.ResourceGraph/resources?api-version=2021-03-01"
-    $GovAssignments = AzAPICall -AzAPICallConfiguration $azapiCallConf -uri $uri -body $payLoad -method 'POST' -listenOn Content
+    $queryResult = AzAPICall -AzAPICallConfiguration $azapiCallConf -uri $uri -body $payLoad -method 'POST' -listenOn Content
+
+    $GovAssignments = [System.Collections.ArrayList]::new()
+
+    foreach ($assignment in $queryResult) {
+
+        $GovAssignmentObj = [GovAssignment]::new()
+        $GovAssignmentObj.CsEnvironment = $CsEnvironment
+        $GovAssignmentObj.SourceType = 'Az'
+        $GovAssignmentObj.AssessmentName = $assignment.assessmentKey
+        $GovAssignmentObj.AssessmentDisplayName = $assignment.displayName
+        $GovAssignmentObj.AssignedResourceId = $assignment.resourceId
+        $GovAssignmentObj.ContainerId = $assignment.resourceId.split("/")[2]
+        $GovAssignmentObj.AssignmentKey = $assignment.assessmentKey
+        $GovAssignmentObj.RemediationDueDate = $assignment.dueDate
+        $GovAssignmentObj.IsGracePeriod = $assignment.isGracePeriod
+        $GovAssignmentObj.Owner = $assignment.owner
+        $GovAssignmentObj.OwnerEmailNotification = $assignment.disableOwnerEmailNotification
+        $GovAssignmentObj.ManagerEmailNotification = $assignment.disableManageremailnotification
+        $GovAssignmentObj.NotificationDayOfWeek = $assignment.notificationDayOfWeek
+
+        # Add the assignment to the list
+        $GovAssignments.add($GovAssignmentObj)
+        $assignment | Add-Member -MemberType NoteProperty -Name "Environment" -Value $CsEnvironment
+    }
 
     return $GovAssignments
 }
